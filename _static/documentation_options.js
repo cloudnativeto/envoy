8cbcdaf02f16685de5be5b1ee9bb0cecf3893fbe
majinghe <majinghe@users.noreply.github.com>
2021-01-13 16:52:15 +0000

    deploy: 04e0e54994897a7361217d976105519065cbd5b5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.16.0-e91bdc',
+    VERSION: '1.16.0-04e0e5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
