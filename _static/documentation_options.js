0db852f24e3c2bec55a975e6a0a2fd7795cac405
cloudnativeto <cloudnativeto@users.noreply.github.com>
2021-03-23 16:22:47 +0000

    deploy: a42d93d77b3532f78ce1712874898d51dc4fcb0c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.16.0-27b0be',
+    VERSION: '1.16.0-a42d93',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
